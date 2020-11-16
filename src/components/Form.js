ed690d0e5d2f40985890724d43ff315346d0c10c
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-11-16 14:53:42 -0600

    made state and controlled input on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,10 @@ const Form = () => {
 		time: '',
 	});
 
+	const handleChange = (event) => {
+		setInput({ ...input, [event.target.name]: event.target.value });
+	};
+
 	const checkForm = (event) => {
 		event.preventDefault();
 		console.log('the form worked', event);
@@ -17,13 +21,25 @@ const Form = () => {
 			<h1>Add A New Song</h1>
 			<form onSubmit={checkForm}>
 				<label for='title'>Title</label>
-				<input type='text' id='title' name='title'></input>
+				<input
+					type='text'
+					id='title'
+					name='title'
+					onChange={handleChange}></input>
 				<br />
 				<label for='artist'>Artist</label>
-				<input type='text' id='artist' name='artist'></input>
+				<input
+					type='text'
+					id='artist'
+					name='artist'
+					onChange={handleChange}></input>
 				<br />
 				<label for='time'>Time</label>
-				<input type='text' id='time' name='time'></input>
+				<input
+					type='text'
+					id='time'
+					name='time'
+					onChange={handleChange}></input>
 				<br />
 				<input type='submit' value='Submit'></input>
 			</form>
